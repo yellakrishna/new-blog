03f0d5739cef755d6e332271c78867bbb7d7eb86
yellakrishna <yellakrishna7675@gmail.com>
2025-07-16 16:10:37 +0530

    fix: added vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173, // or any port you like
-  },
-});
+  base: "./",
+})
